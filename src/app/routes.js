cdce9a8bfa8e8dd845c472789acefef1042db944
Marcos Moura <marcosvmmoura@gmail.com>
2017-03-24 17:18:42 -0300

    simplify routes

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,4 +1,15 @@
-import HomeRoutes from './pages/Home'
-import ErrorRoutes from './pages/Error'
+const Home = r => require.ensure([], () => r(require('./pages/Home/Home.vue')), 'home')
+const ErrorPage = r => require.ensure([], () => r(require('./pages/Error/Error.vue')), 'error')
 
-export default [...HomeRoutes, ...ErrorRoutes]
+export default [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '*',
+    name: 'error',
+    component: ErrorPage
+  }
+]
